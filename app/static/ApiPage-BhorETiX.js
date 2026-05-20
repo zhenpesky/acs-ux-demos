88cbd11fddd9a903d752a8dbefc51e14e1405bfa
zhenpesky <zhenpesky@users.noreply.github.com>
2026-05-20 14:27:11 -0700

    Deploy: consolidate to app/ base path, remove saved-filters/

---
+++ app/static/ApiPage-BhorETiX.js
@@ -0,0 +1,1 @@
+import{t as r}from"./index-Ct2phOu-.js";import{S as o}from"./SwaggerBrowser-DeFPsZqS.js";import"./react-pF2EnNv3.js";import"./apollo-BxVF6eGb.js";import"./lodash-JMWJiBov.js";function e(){return r.jsx(o,{uri:"/api/docs/swagger"})}export{e as default};
--- app/static/ApiPage-ic4x9lv2.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./index-EQPe2kmx.js";import{S as o}from"./SwaggerBrowser-30MWwQ7T.js";import"./react-pF2EnNv3.js";import"./apollo-BxVF6eGb.js";import"./lodash-JMWJiBov.js";function e(){return r.jsx(o,{uri:"/api/docs/swagger"})}export{e as default};

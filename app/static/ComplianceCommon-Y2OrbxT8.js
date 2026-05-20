195a9d8ce7d37bf0db124d62a9bc37f3e08cbc67
zhenpesky <zhenpesky@users.noreply.github.com>
2026-05-20 15:15:24 -0700

    Deploy: consolidate to app/ base path, remove saved-filters/

---
+++ app/static/ComplianceCommon-Y2OrbxT8.js
@@ -0,0 +1,1 @@
+import{dX as i,cb as c,a1 as u}from"./index-B5c9d9EO.js";const l="/v2/compliance";function y({page:a,perPage:r,sortOption:e,searchFilter:t={}}){const s=i(t),o=c({page:a,perPage:r,sortOption:e}),n={query:{query:s,pagination:o}};return u.stringify(n,{arrayFormat:"repeat",allowDots:!0})}export{y as b,l as c};

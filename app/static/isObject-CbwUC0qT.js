662f3590fe85068ae54b61cb4b67b4f461795c44
zhenpesky <zhenpesky@users.noreply.github.com>
2026-05-20 14:13:08 -0700

    Deploy: consolidate to app/ base path, remove saved-filters/

---
+++ app/static/isObject-CbwUC0qT.js
@@ -0,0 +1,1 @@
+import{g as t}from"./lodash-JMWJiBov.js";import{gt as r}from"./index-DhnQ6Mba.js";var o=r();const i=t(o);export{i};
--- app/static/isObject-Do_N551Y.js
+++
@@ -1,1 +0,0 @@
-import{g as t}from"./lodash-JMWJiBov.js";import{gt as r}from"./index-Cu0j0Jth.js";var o=r();const i=t(o);export{i};

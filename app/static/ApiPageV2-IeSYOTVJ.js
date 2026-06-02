15a70598458f36d060905087ef347242f477c0ff
zhenpesky <zhenpesky@users.noreply.github.com>
2026-06-02 16:19:06 -0700

    Deploy: 2026-06-02 16:19

---
+++ app/static/ApiPageV2-IeSYOTVJ.js
@@ -0,0 +1,1 @@
+import{w as r}from"./index-kZ3yQhCS.js";import{S as o}from"./SwaggerBrowser-CYfM914h.js";import"./react-pF2EnNv3.js";import"./apollo-BxVF6eGb.js";import"./lodash-JMWJiBov.js";function e(){return r.jsx(o,{uri:"/api/docs/v2/swagger"})}export{e as default};

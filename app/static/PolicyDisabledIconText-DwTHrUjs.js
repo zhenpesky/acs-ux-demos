78e855cecf14dcf82a87be43378aa3b379ca07e9
Zhenpeng Chen <zhenpesky@users.noreply.github.com>
2026-06-17 11:02:38 -0700

    Deploy: 2026-06-17 11:02

---
+++ app/static/PolicyDisabledIconText-DwTHrUjs.js
@@ -0,0 +1,1 @@
+import{w as n,cw as e,fF as x,au as a}from"./index-rApNR5XO.js";function r({isDisabled:o,isTextOnly:s}){const t=o?n.jsx(x,{}):n.jsx(a,{}),c=o?"Disabled":"Enabled";return n.jsx(e,{icon:t,text:c,isTextOnly:s})}export{r as P};

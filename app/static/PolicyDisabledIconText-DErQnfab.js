3cfb3e33683d69722b60d99a3bfae396257abd2d
zhenpesky <zhenpesky@users.noreply.github.com>
2026-06-17 07:15:05 -0700

    Deploy: 2026-06-17 07:15

--- app/static/PolicyDisabledIconText-DErQnfab.js
+++
@@ -1,1 +0,0 @@
-import{w as n,cw as e,fF as x,au as a}from"./index-DgtMY6ht.js";function r({isDisabled:o,isTextOnly:s}){const t=o?n.jsx(x,{}):n.jsx(a,{}),c=o?"Disabled":"Enabled";return n.jsx(e,{icon:t,text:c,isTextOnly:s})}export{r as P};
---
+++ app/static/TraitsOriginLabel-B2c1FBPX.js
@@ -0,0 +1,1 @@
+import{b9 as a,w as s,aY as n,hG as e}from"./index-BDaXGazn.js";function l({traits:i}){const r=a(i),o=e[r];return s.jsx(n,{color:o,children:r})}export{l as T};

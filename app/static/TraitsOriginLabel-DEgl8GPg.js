fefc57bd2edcea0e08962f4e9a6a4e4ac2d26f66
zhenpesky <zhenpesky@users.noreply.github.com>
2026-06-16 20:47:25 -0700

    Deploy: 2026-06-16 20:47

--- app/static/TraitsOriginLabel-DEgl8GPg.js
+++
@@ -1,1 +0,0 @@
-import{b9 as a,w as s,aY as n,hG as e}from"./index-fVO23mMv.js";function l({traits:i}){const r=a(i),o=e[r];return s.jsx(n,{color:o,children:r})}export{l as T};
---
+++ app/static/isObject-B0nfbXQb.js
@@ -0,0 +1,1 @@
+import{g as r}from"./lodash-JMWJiBov.js";import{gB as t}from"./index-BQ9_wOok.js";var o=t();const i=r(o);export{i};

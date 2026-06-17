028133eed6865198ed72bb594e6ef4c7e7e28511
Zhenpeng Chen <zhenpesky@users.noreply.github.com>
2026-06-17 10:10:48 -0700

    Deploy: 2026-06-17 10:10

--- app/static/ApiPageV2-BfTr6nsX.js
+++
@@ -1,1 +0,0 @@
-import{w as r}from"./index-HurMG9yl.js";import{S as o}from"./SwaggerBrowser-DC2CSR_j.js";import"./react-pF2EnNv3.js";import"./apollo-BxVF6eGb.js";import"./lodash-JMWJiBov.js";function e(){return r.jsx(o,{uri:"/api/docs/v2/swagger"})}export{e as default};
---
+++ app/static/TraitsOriginLabel-CLj9Yw8C.js
@@ -0,0 +1,1 @@
+import{b9 as a,w as s,aY as n,hG as e}from"./index-C8sEyDBX.js";function l({traits:i}){const r=a(i),o=e[r];return s.jsx(n,{color:o,children:r})}export{l as T};

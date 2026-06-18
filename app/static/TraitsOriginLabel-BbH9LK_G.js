62ca6050d56b528bb8f0c770744f7a3458e760a6
Zhenpeng Chen <zhenpesky@users.noreply.github.com>
2026-06-18 00:35:45 -0700

    Deploy: 2026-06-18 00:35

---
+++ app/static/TraitsOriginLabel-BbH9LK_G.js
@@ -0,0 +1,1 @@
+import{ba as o,w as s,aZ as n,gM as e}from"./index-CGe-NB7Y.js";function l({traits:a}){const r=o(a),i=e[r];return s.jsx(n,{color:i,children:r})}export{l as T};
--- app/static/isObject-Cz49zpxj.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./lodash-JMWJiBov.js";import{gC as t}from"./index-L7fUJikH.js";var o=t();const i=r(o);export{i};

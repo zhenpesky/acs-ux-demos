248c73e93f9b37c38cf87bcbd243eadb7265b66c
Zhenpeng Chen <zhenpesky@users.noreply.github.com>
2026-06-17 20:52:22 -0700

    Deploy: 2026-06-17 20:52

---
+++ app/static/TraitsOriginLabel-DOiGePT1.js
@@ -0,0 +1,1 @@
+import{ba as o,w as s,aZ as n,gM as e}from"./index-k0J4Ew6c.js";function l({traits:a}){const r=o(a),i=e[r];return s.jsx(n,{color:i,children:r})}export{l as T};
---
+++ app/static/_castFunction-0LU7B8F5.js
@@ -0,0 +1,1 @@
+import{gs as u}from"./index-k0J4Ew6c.js";var t,r;function o(){if(r)return t;r=1;var i=u();function e(n){return typeof n=="function"?n:i}return t=e,t}export{o as r};

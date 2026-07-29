55ff077392deb8baef43e3906f2a45a45fea6034
Zhenpeng Chen <zhenpesky@users.noreply.github.com>
2026-07-29 12:48:56 -0700

    Deploy: 2026-07-29 12:48

--- app/static/InnerScrollContainer-CTbeuyAB.js
+++
@@ -1,1 +0,0 @@
-import{b as n}from"./apollo-BxVF6eGb.js";import{w as l,a6 as o,gZ as c}from"./index-BER86R8m.js";const t=s=>{var{children:r,className:e}=s,a=n(s,["children","className"]);return l.jsx("div",Object.assign({className:o(e,c.scrollInnerWrapper)},a,{children:r}))};t.displayName="InnerScrollContainer";export{t as I};
